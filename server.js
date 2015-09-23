c42ddb33024b4cb1638368a7707aa6c41bf9ad52
Daiki Yano <ht12a092@gmail.com>
2015-09-23 15:26:49 +0900

    Add: first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+	server
+**/
+var express = require('express');
+var app = express();
+
+// wwwディレクトリを静的ファイルディレクトリとして登録
+app.use(express.static('www'));
+
+// サーバを開始
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Server listening at http://%s:%s', host, port);
+});
+
+var socketIO = require('socket.io');
+var scoa_io = socketIO.listen(8081);
